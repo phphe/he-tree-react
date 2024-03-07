fa516b66d0a90b8cf4c684bb3610e43b27af429f
phphe <phphe@outlook.com>
2024-03-08 01:42:50 +0800

    docs: enable search

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -6,6 +6,9 @@ export default defineConfig({
   description: "A VitePress Site",
   cleanUrls: true,
   themeConfig: {
+    search: {
+      provider: "local",
+    },
     // https://vitepress.dev/reference/default-theme-config
     // nav: [
     //   { text: "Home", link: "/" },
