7f117a1cbbe8799585bf72ac6c135e4e9514c45b
phphe <phphe@outlook.com>
2026-02-27 16:19:19 +0800

    docs: add demo video

---
+++ apps/docs/.vitepress/theme/MyLayout.vue
@@ -0,0 +1,22 @@
+<script setup>
+import DefaultTheme from 'vitepress/theme'
+
+const { Layout } = DefaultTheme
+</script>
+
+<template>
+  <Layout>
+    <template #home-hero-image>
+      <video autoplay loop muted playsinline
+        style="width: 350px; max-width: 100%; height: auto; display: block; margin: 0 auto;">
+        <source src="/demo.webm" type="video/webm">
+      </video>
+    </template>
+  </Layout>
+</template>
+
+<style>
+.VPHero .image-bg {
+  display: none;
+}
+</style>
--- apps/docs/.vitepress/theme/index.ts
+++ apps/docs/.vitepress/theme/index.ts
@@ -4,14 +4,11 @@ import type { Theme } from "vitepress";
 import DefaultTheme from "vitepress/theme";
 import "./style.css";
 import DemoIframe from "../../components/DemoIframe.vue";
+import MyLayout from './MyLayout.vue'
 
 export default {
   extends: DefaultTheme,
-  Layout: () => {
-    return h(DefaultTheme.Layout, null, {
-      // https://vitepress.dev/guide/extending-default-theme#layout-slots
-    });
-  },
+  Layout: MyLayout,
   enhanceApp({ app, router, siteData }) {
     // ...
     app.component("DemoIframe", DemoIframe);
